d7f684e26dda5671ceb287a4adad5e9c043a1e1d
homelet <homeletwei@gmail.com>
2020-07-24 16:14:27 -0400

    Commit

--- application.py
+++ application.py
@@ -1,11 +1,14 @@
+from os import environ
+
 from flask import Flask
 from flask_script import Manager
-
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config.from_pyfile("config/base_setting.py")
+if "ops_config" in environ:
+    app.config.from_pyfile("config/{}_setting.py".format(environ["ops_config"]))
 
 db = SQLAlchemy(app)
 
